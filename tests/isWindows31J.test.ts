424615d36d357a6c7e1ced1f73c79d6ddee3ec31
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2023-04-18 07:25:12 +0900

    Improve code

--- tests/isWindows31J.test.ts
+++ tests/isWindows31J.test.ts
@@ -2,20 +2,25 @@ import { describe, expect, test } from 'vitest'
 import isWindows31J from '../src/isWindows31J.js'
 
 describe('isWindows31J', () => {
-    test("test ascii", () => {
-        for (let i = 0x00; i <= 0x7F; i++) {
-            expect(isWindows31J(String.fromCharCode(i))).toBe(true)
-        }
-    })
 
-    test("test fullwidth symbol", () => {
-        expect(isWindows31J("　")).toBe(true)
-        expect(isWindows31J("、")).toBe(true)
-        expect(isWindows31J("、")).toBe(true)
-    })
+  test.each((() => {
+      const asciiList = new Array<string>()
+      for (let i = 0x00; i <= 0x7F; i++) {
+        asciiList.push(String.fromCharCode(i))
+      }
+      return asciiList
+  })())("test ascii '%s'", (c) => {
+      expect(isWindows31J(c)).toBe(true)
+  })
 
-    test("test fullwidth kanji", () => {
-        expect(isWindows31J("亜")).toBe(true)
-        expect(isWindows31J("腕")).toBe(true)
-    })
+  test("test fullwidth symbol", () => {
+    expect(isWindows31J("　")).toBe(true)
+    expect(isWindows31J("、")).toBe(true)
+    expect(isWindows31J("￦")).toBe(false)
+  })
+
+  test("test fullwidth kanji", () => {
+    expect(isWindows31J("亜")).toBe(true)
+    expect(isWindows31J("腕")).toBe(true)
+  })
 })
