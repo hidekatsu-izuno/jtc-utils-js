92f65a03f20abacb51c6cab5e662d530e827d5d8
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2023-04-19 08:31:12 +0900

    Improve code

--- src/isJapaneseFullwidth.ts
+++
@@ -1,7 +0,0 @@
-export default function isJapaneseFullwidth(value: any) {
-  if (!value || typeof value !== "string") {
-    return false
-  }
-
-  return /^\p{Script_Extensions=Han}+$/.test(value)
-}
--- src/isJapaneseHalfwidth.ts
+++
@@ -1,7 +0,0 @@
-export default function isJapaneseHalfwidth(value: any) {
-  if (!value || typeof value !== "string") {
-    return false
-  }
-
-  return /^[\x20-\x7F\uFF61-\uFF9F]+$/.test(value)
-}
