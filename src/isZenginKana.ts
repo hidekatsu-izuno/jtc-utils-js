3f655b9e6b5c21f0b1607a2850ad20e05bf347d9
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2023-04-22 00:54:43 +0900

    Improve code

--- src/isZenginKana.ts
+++ src/isZenginKana.ts
@@ -1,7 +1,24 @@
-export default function isZenginKana(value: any) {
+import { CharRangeOptions } from "./CharRangeOption.js"
+
+export default function isZenginKana(value: any, options?: CharRangeOptions) {
   if (!value || typeof value !== "string") {
     return false
   }
 
-  return /^[\x200-9A-Z()/.\\\uFF62\uFF63\uFF71-\uFF9F-]+$/.test(value)
+  const result = /^[\r\n\x200-9A-Z()/.\\\uFF62\uFF63\uFF71-\uFF9F-]+$/.test(value)
+  if (result) {
+    // Default excludes
+    if (options?.linebreak !== true && /[\r\n]/.test(value)) {
+      return false
+    }
+
+    // Default includes
+    if (options?.punct === false && /[!-/:-@[-`{-~\uFF61-\uFF65]/u.test(value)) {
+      return false
+    }
+    if (options?.space === false && /[\x20]/.test(value)) {
+      return false
+    }
+  }
+  return result
 }
