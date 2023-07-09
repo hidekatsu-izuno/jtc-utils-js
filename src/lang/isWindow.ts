691ab52745b096fc4668496abb827b082c6bdc38
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2023-07-09 12:23:57 +0900

    Improve code

--- src/lang/isWindow.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Derieved from jQuery.isWindow (https://github.com/jquery/jquery)
- * @license MIT License
- */
-export function isWindow(obj: any) {
-  return obj != null && obj === obj.window
-}
--- src/util/PackedMap.ts
+++ src/util/PackedMap.ts
@@ -3,66 +3,104 @@ export interface PackedMapSetter {
 }
 
 export class PackedMap {
-  private map = new Map<number, Uint16Array | Uint32Array>()
+  private map: Map<number, Uint8Array | Uint16Array | Uint32Array>
   private init?: (setter: PackedMapSetter) => void
 
-  constructor(init: (setter: PackedMapSetter) => void) {
-    this.init = init
+  constructor(init: (setter: PackedMapSetter) => void | Map<number, Uint8Array | Uint16Array | Uint32Array>) {
+    if (init instanceof Map) {
+      this.map = init
+    } else {
+      this.map = new Map<number, Uint8Array | Uint16Array | Uint32Array>()
+      this.init = init
+    }
   }
 
   initialize() {
     if (!this.init) {
-      return
+      return this.map
     }
 
     const map = new Map<number, number[]>()
 
     this.init(<PackedMapSetter> {
       set(key: number, value: number) {
-        const key1 = key >>> 4
-        const key2 = key & 0xF
+        const key1 = key >>> 5
+        const key2 = key & 0b11111
 
         let array = map.get(key1)
         if (!array) {
-          array = [0]
+          array = []
           map.set(key1, array)
         }
-        if (!array[key2 + 1]) {
-          array[0] = array[0] | (1 << (15 - key2))
-          array[key2 + 1] = value
+        if (!array[key2]) {
+          array[key2] = value
         }
       }
     })
 
     for (const [key, value] of map.entries()) {
-      let max = 2
-      const filtered = value.filter((item?: number) => {
+      let flags = 0
+      let max = 1
+      const filtered = value.filter((item, index) => {
         if (item != null) {
+          flags |= (1 << (31 - index))
           if ((item & 0xFFFF0000) !== 0) {
             max = Math.max(max, 4)
+          } else if ((item & 0xFFFFFF00) !== 0) {
+            max = Math.max(max, 2)
           }
           return true
         }
         return false
       })
-      this.map.set(key, max === 2 ? new Uint16Array(filtered)
-        : new Uint32Array(filtered)
-      )
+      if (max === 1) {
+        const array = new Uint8Array(filtered.length + 4)
+        array[0] = (flags >>> 24) & 0xFF
+        array[1] = (flags >>> 16) & 0xFF
+        array[2] = (flags >>> 8) & 0xFF
+        array[3] = flags & 0xFF
+        array.set(filtered, 4)
+        this.map.set(key, array)
+      } else if (max === 2) {
+        const array = new Uint16Array(filtered.length + 2)
+        array[0] = (flags >>> 16) & 0xFFFF
+        array[1] = flags & 0xFFFF
+        array.set(filtered, 2)
+        this.map.set(key, array)
+      } else {
+        const array = new Uint32Array(filtered.length + 1)
+        array[0] = flags
+        array.set(filtered, 1)
+        this.map.set(key, array)
+      }
     }
 
     this.init = undefined
+    return this.map
   }
 
   get(key: number): number | undefined {
     this.initialize()
 
-    const key1 = key >>> 4
-    const key2 = key & 0xF
+    const key1 = key >>> 5
+    const key2 = key & 0b11111
     const array = this.map?.get(key1)
     if (array) {
-      const shifted = array[0] >>> (15 - key2)
-      if ((shifted & 0x1) !== 0) {
-        return array[bitcount(shifted)]
+      if (array.BYTES_PER_ELEMENT === 1) {
+        const shifted = (array[0] << 24 | array[1] << 16 | array[2] << 8 | array[3]) >>> (31 - key2)
+        if ((shifted & 0x1) !== 0) {
+          return array[bitcount(shifted) - 1 + 4]
+        }
+      } else if (array.BYTES_PER_ELEMENT === 2) {
+        const shifted = (array[0] << 16 | array[1]) >>> (31 - key2)
+        if ((shifted & 0x1) !== 0) {
+          return array[bitcount(shifted) - 1 + 2]
+        }
+      } else {
+        const shifted = array[0] >>> (31 - key2)
+        if ((shifted & 0x1) !== 0) {
+          return array[bitcount(shifted) - 1 + 1]
+        }
       }
     }
   }
