ea067a937089e02f2db4b178a748f51740996141
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2025-04-16 06:07:55 +0900

    Update packages

--- tests/formatNumber.test.ts
+++ tests/formatNumber.test.ts
@@ -21,7 +21,7 @@ describe("formatNumber", () => {
     expect(formatNumber(-10000, "###,###.#")).toBe("-10,000");
     expect(formatNumber(-10000, "###,###.0")).toBe("-10,000.0");
     expect(formatNumber(-10000, "##,##,##.#")).toBe("-1,00,00");
-    
+
     expect(formatNumber(-10000, "0000000")).toBe("-0010000");
     expect(formatNumber(10000, "0,000,000")).toBe("0,010,000");
     expect(formatNumber(-10000, "0,000,000.#")).toBe("-0,010,000");
